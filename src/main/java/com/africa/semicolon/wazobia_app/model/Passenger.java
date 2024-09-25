16dfaf57a0489e998511e6c70328d8e0acd9ffe0
nelson <nelsonakewe0@gmail.com>
2024-09-25 12:46:18 +0100

    model created

---
+++ src/main/java/com/africa/semicolon/wazobia_app/model/Passenger.java
@@ -0,0 +1,9 @@
+package com.africa.semicolon.wazobia_app.model;
+
+public class Passenger {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
