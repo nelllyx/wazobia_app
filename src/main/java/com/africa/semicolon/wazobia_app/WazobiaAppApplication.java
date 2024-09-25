f60233b5459bf7cfe6ae2929ff1c80df06bc5bf9
nelson <nelsonakewe0@gmail.com>
2024-09-25 12:37:27 +0100

    first commit

---
+++ src/main/java/com/africa/semicolon/wazobia_app/WazobiaAppApplication.java
@@ -0,0 +1,13 @@
+package com.africa.semicolon.wazobia_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WazobiaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WazobiaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/africa/semicolon/wazobia_app/WazobiaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.africa.semicolon.wazobia_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WazobiaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
