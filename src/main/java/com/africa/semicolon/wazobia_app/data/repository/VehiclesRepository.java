60740b043ec990137d73a6f8617a63634e7f82c0
funso1234 <samuel08063251499@gmail.com>
2024-09-26 12:32:51 +0100

    ff

--- src/main/java/com/africa/semicolon/wazobia_app/data/repository/VehiclesRepository.java
+++ src/main/java/com/africa/semicolon/wazobia_app/data/repository/VehiclesRepository.java
@@ -3,5 +3,5 @@ package com.africa.semicolon.wazobia_app.data.repository;
 import com.africa.semicolon.wazobia_app.data.model.Vehicles;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VehiclesRepository extends JpaRepository<Vehicles ,IllegalAccessError> {
+public interface VehiclesRepository extends JpaRepository<Vehicles ,Long> {
 }
